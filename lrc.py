1797f214ce7c78ec5c9e048bcf49fc3af32026a5
cjrzh <cjrzh@163.com>
2016-12-25 21:27:51 +0800

    add .py

---
+++ lrc.py
@@ -0,0 +1,33 @@
+import re
+import urllib.request
+
+def transform(source):
+    '''
+    transform lyrics into the form that MediaGo can read
+    '''
+    result=[]
+    for line in source:
+        if bool(re.search(r'\[\d\d.\d\d.\d\d\d\]',line)):
+            res=line[:9]+line[10:]
+            result.append(res)
+        else :
+            result.append(line)
+    return result
+
+def fetch(id):
+    result=[]
+    url="http://music.163.com/api/song/media?id="+str(id)
+    response=urllib.request.urlopen(url)
+    html = response.read()
+    return html.decode('utf-8')
+    #return result
+
+# test fetch
+res=fetch(418603077)
+import json
+res_dict = json.loads(res)
+list_res=res_dict["lyric"].split('\n')
+#print(list_res)
+trans_res=transform(list_res)
+for line in trans_res:
+    print(line)
